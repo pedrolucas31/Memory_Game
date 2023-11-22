9b0fee9265dd1de6992ee89c40621fcc607a4032
Pedro Lucas <pedrolucas13083110@gmail.com>
2023-11-22 18:41:40 -0300

    add base project

---
+++ scripts/cards.js
@@ -0,0 +1,5 @@
+const playerName = document.querySelector(".playerName");
+
+const storagePlayerName = localStorage.getItem("@memoryGame:playerName");
+
+playerName.innerHTML = storagePlayerName;
---
+++ scripts/newGame.js
@@ -0,0 +1,21 @@
+const inputName = document.querySelector(".inputName");
+const newGameButton = document.querySelector(".newGameButton");
+const newGameForm = document.querySelector(".newGameForm");
+
+function validateInput(event) {
+  if (event.target.value.length >= 3) {
+    newGameButton.removeAttribute("disabled");
+  } else {
+    newGameButton.setAttribute("disabled", "true");
+  }
+}
+
+function handleSubmitNewGame(event) {
+  event.preventDefault();
+  localStorage.setItem("@memoryGame:playerName", inputName.value);
+  inputName.value = "";
+  window.location.href = "pages/cards.html";
+}
+
+inputName.addEventListener("input", validateInput);
+newGameForm.addEventListener("submit", handleSubmitNewGame);
