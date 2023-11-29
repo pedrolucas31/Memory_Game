1c1ede31ebefa1c5006c91894db416ce26fda45f
Pedro Lucas <pedrolucas13083110@gmail.com>
2023-11-29 19:28:11 -0300

    add button navigate to rank

--- scripts/newGame.js
+++ scripts/newGame.js
@@ -1,6 +1,3 @@
-const inputName = document.querySelector(".inputName");
-const newGameButton = document.querySelector(".newGameButton");
-const newGameForm = document.querySelector(".newGameForm");
 
 function validateInput(event) {
   if (event.target.value.length >= 3) {
@@ -17,5 +14,15 @@ function handleSubmitNewGame(event) {
   window.location.href = "pages/cards.html";
 }
 
+function navigateToRank() {
+  window.location.href = "pages/rank.html";
+}
+
+const inputName = document.querySelector(".inputName");
+const newGameButton = document.querySelector(".newGameButton");
+const newGameForm = document.querySelector(".newGameForm");
+const buttonRank = document.querySelector(".buttonRank");
+
 inputName.addEventListener("input", validateInput);
 newGameForm.addEventListener("submit", handleSubmitNewGame);
+buttonRank.addEventListener("click", navigateToRank);
